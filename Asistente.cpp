f6757e9baa9ac8a78680f6249a77c9f8b4c0b955
lavaca777 <107744299+lavaca777@users.noreply.github.com>
2024-04-05 17:03:22 -0300

    Add files via upload

---
+++ Asistente.cpp
@@ -0,0 +1,42 @@
+#include "Asistente.h"
+#include <iostream>
+using namespace std;
+
+Asistente::Asistente(string nombre, int edad, int dinero, string universidad, bool esAlumno, bool esProfesor) {
+    this->nombre = nombre;
+    this->edad = edad;
+    this->dinero = dinero;
+    this->universidad = universidad;
+    this->esAlumno = esAlumno;
+    this->esProfesor = esProfesor;
+}
+
+Asistente::~Asistente() {}
+
+void Asistente::mostrarDetalles() {
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Dinero: " << dinero << endl;
+    cout << "Universidad: " << universidad << endl;
+    cout << "Es alumno: " << (esAlumno ? "Si" : "No") << endl;
+    cout << "Es profesor: " << (esProfesor ? "Si" : "No") << endl;
+}
+
+void Asistente::guardarEnArchivo(const string& nombreArchivo) {
+    std::ofstream archivo(nombreArchivo, std::ios::app);
+    if (archivo.is_open()) {
+        archivo << nombre << "," << edad << "," << dinero << "," << universidad << "," << esAlumno << "," << esProfesor << "\n";
+        archivo.close();
+    } else {
+        std::cerr << "Error: No se pudo abrir el archivo para guardar el asistente.\n";
+    }
+}
+
+
+// Getters
+string Asistente::getNombre() { return nombre; }
+int Asistente::getEdad() { return edad; }
+int Asistente::getDinero() { return dinero; }
+string Asistente::getUniversidad() { return universidad; }
+bool Asistente::esEstudiante() { return esAlumno; }
+bool Asistente::esDocente() { return esProfesor; }
